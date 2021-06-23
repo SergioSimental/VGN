a6bbbfd2bdb19c3a2a9f8f80f2f0c4c03a5f8802
SergioSimental <sergios3456@gmail.com>
2021-06-23 14:38:17 -0700

    Update to article data storage

---
+++ Run_Python_Script.bat
@@ -0,0 +1,2 @@
+"C:\Users\steve\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\steve\Desktop\VGN\newsapp\news_api\scripts.py"
+pause
--- news_api/views.py
+++ news_api/views.py
@@ -2,21 +2,11 @@ from django.shortcuts import render
 import requests
 from datetime import date
 import json
-from pathlib import Path
+import time
 
-API_KEY = 'NewAPI_KEY'
+
+API_KEY = '91bbdf9adfeb4c0f8e046fb4e1e58933'
 DATE = date.today()
-file = '/newsapp/news_api/news.json'
-json_news = { }
-
-
-if Path('news.json').is_file():
-    print ("File exist")
-else:
-    print ("File not exist")
-    with open('news.json', 'w') as outfile:
-        json.dump(json_news, outfile)
-
 
 # Create your views here.
 
@@ -24,12 +14,6 @@ def home(request):
     url = f'https://newsapi.org/v2/everything?q=gaming&from={DATE}&sortBy=popularity&language=en&apiKey={API_KEY}'
     response = requests.get(url)
     data = response.json()
-    
-    #write_json(data,file)
-
-
-    #ds = json.loads(file)
-    #unique_news = { each['obj_id'] : each for each in ds}.values()
 
     articles = data['articles']
     
@@ -43,13 +27,17 @@ def archive(request):
     url = f'https://newsapi.org/v2/everything?q=gaming&from={DATE}&sortBy=popularity&language=en&apiKey={API_KEY}'
     response = requests.get(url)
     data = response.json()
-    json_news.update(data)
+    
+    
+    with open('data.json', 'r') as fp:
+        obj = json.load(fp)
 
-    articles = json_news['articles']
+    articles = obj['articles']
 
     context={
         'articles' : articles
     }
+
 
     return render(request, 'news/archive.html', context)
 
@@ -57,9 +45,8 @@ def archive(request):
 def search(request):
     if request.method == 'GET':
         query = request.GET.get('search')
-        url = f'https://newsapi.org/v2/everything?q=gaming+q={query}&sortBy=popularity&language=en&apiKey={API_KEY}'
-        print(url)
-        response = requests.get(url)
+        url1 = f'https://newsapi.org/v2/everything?q=gaming+q={query}&sortBy=popularity&language=en&apiKey={API_KEY}'
+        response = requests.get(url1)
         data = response.json()
 
         articles= data['articles']
@@ -71,15 +58,4 @@ def search(request):
         return render(request, 'news/search.html', context)
 
 
-
-def write_json(new_data, file_name):
-    with open(file, 'r+') as news:
-        # First we load existing data into a dict.
-        file_data = json.load(file_name)
-        # Join new_data with file_data
-        file_data.update(new_data)
-        # Sets file's current position at offset.
-        file_name.seek(0)
-        # convert back to json.
-        json.dump(file_data, file_name, indent=4)
 
